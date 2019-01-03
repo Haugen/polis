909b7e0d5e620784c521f26da6fc03523d659cc9
Tobias Haugen <hej@tobiashaugen.se>
2019-01-03 10:01:45 -0500

    Setting timestamps and fetching hourly.

--- src/polis/polis.model.js
+++ src/polis/polis.model.js
@@ -2,21 +2,24 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const eventSchema = new Schema({
-  id: {
-    type: String,
-    unique: true,
-    dropDups: true
-  },
-  datetime: String,
-  name: String,
-  summary: String,
-  url: String,
-  type: String,
-  location: {
+const eventSchema = new Schema(
+  {
+    id: {
+      type: String,
+      unique: true,
+      dropDups: true
+    },
+    datetime: String,
     name: String,
-    gps: String
-  }
-});
+    summary: String,
+    url: String,
+    type: String,
+    location: {
+      name: String,
+      gps: String
+    }
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Event', eventSchema);
